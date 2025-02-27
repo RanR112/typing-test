53b856805c87203223c12bc89374dd5ae4d30363
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-02-27 19:18:31 +0700

    Add texts

--- src/global/Texts.js
+++ src/global/Texts.js
@@ -3,7 +3,22 @@ const Texts = [
     { id: 2, text: 'Ku coba jalani hari dengan pengganti dirimu, tapi hatiku selalu berpihak lagi padamu.' },
     { id: 3, text: 'Dan kau hadir merubah segalanya menjadi lebih indah. Kau bawa cintaku setinggi angkasa. Membuatku merasa sempurna dan membuatku utuh, tuk menjalani hidup.' },
     { id: 4, text: 'Seandainya saja ku mampu memulihkan perasaanmu yang tlah lama terluka oleh dirinya. Lupakan semua, sudahlah cukup di sini.' },
-    { id: 5, text: 'Lumpuhkanlah ingatanku, hapuskan tentang dia. Hapuskan memoriku tentang dia. Hilangkanlah ingatanku jika itu tentang dia. Ku ingin ku lupakannya.' }
+    { id: 5, text: 'Lumpuhkanlah ingatanku, hapuskan tentang dia. Hapuskan memoriku tentang dia. Hilangkanlah ingatanku jika itu tentang dia. Ku ingin ku lupakannya.' },
+    { id: 6, text: 'Aku ingin dirimu yang menjadi milikku bersamaku mulai hari ini. Hilang ruang untuk cinta yang lain separuh jalan pernah dilewati. Meski ada kecewa aku yang dulu tak begitu lagi. Takkan ku ulangi jangan dulu engkau berpaling. Beri ku kesempatan aku ingin dirimu tetap jadi milikku.'},
+    { id: 7, text: 'Tak akan ada cinta yang lain pastikan cintaku hanya untukmu. Pernahkah terbersit olehmu? Aku pun takut kehilangan dirimu.'},
+    { id: 8, text: 'Bila di depan nanti banyak cobaan untuk kisah cinta kita, jangan cepat menyerah. Kau punya aku, kupunya kamu, selamanya Akan begitu. Tetaplah bersamaku jadi teman hidupku berdua kita hadapi dunia. Kau milikku, milikmu kita satukan tuju bersama arungi derasnya waktu.'},
+    { id: 9, text: 'Teruslah bermimpi, walau kenyataannya jauh berbeda. Percayalah, lelah ini hanya sebentar saja. Jangan menyerah, walaupun tak mudah meraihnya.'},
+    { id: 10, text: 'Cintai aku seperti yang kau lakukan, la - la - cintai aku seperti yang kau lakukan.'},
+    { id: 11, text: 'Akulah Arjuna yang mencari cinta. Wahai wanita cintailah aku'},
+    { id: 12, text: 'Cintaku ini bukan cinta milik manusia biasa. Cintaku ini cinta sejati yang paling sejati'},
+    { id: 13, text: 'Jangan pergi dari hidupku tak sanggup harus hidup tanpamu. Karna jauh lebih indah bila kita bersama, seperti yang terjadi kemarin.'},
+    { id: 14, text: 'Kau jadikan aku kekasih bayangan untuk menemani saat kau merasa sepi. Bertahun lamanya kujalani kisah cinta sendiri.'},
+    { id: 15, text: 'Kaulah bentuk terindah dari baiknya Tuhan padaku. Waktu tak mengusaikan cantikmu, kau wanita terhebat bagiku, tolong kamu camkan itu.'},
+    { id: 16, text: 'Simpan mawar yang kuberi mungkin wanginya mengilhami. Sudikah dirimu untuk kenali aku dulu. Sebelum kau ludahi aku, sebelum kau robek hatiku.'},
+    { id: 17, text: 'Kau kembali kepada sunyi kuikhlaskan semua pada takdir, Tuhan pasti tahu yang terbaik untukku. Tenanglah hatiku, tegarlah diri walaupun perih berjalanlah lagi sejauh mungkin hingga suatu hari nanti pasti bertemu dengan hati yang tak menyakiti.'},
+    { id: 18, text: 'Pergilah kasih kejarlah keinginanmu selagi masih ada waktu. Jangan hiraukan diriku yang rela berpisah demi untuk dirimu. Semoga tercapai segala keinginanmu.'},
+    { id: 19, text: 'Entah di mana dirimu berada hampa terasa hidupku tanpa dirimu. Apakah di sana kau rindukan aku? Seperti diriku yang selalu merindukanmu.'},
+    { id: 20, text: 'Penantian ini teramatlah panjang. Coba kau rasakan sayang letihku di ujung jalan. Dia menghilang membawa semua kenangan terindah yang ku rasakan saat bersamanya sayang.'}
 ]
 
 export default Texts;
